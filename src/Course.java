f4aae3a3c18ef3136e700574f30e13d42a4c12f3
BT_4N2_07 <tdukdak9287@bths.edu>
2023-05-03 12:14:42 -0400

    started my Course class

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,14 @@
 public class Course {
+    private String courseName;
+    private int grade;
+    private String comment;
+    private double weight;
+
+    public Course(String courseName, int grade, String comment, double weight){
+        this.courseName = courseName;
+        this.grade = grade;
+        this.comment = comment;
+        this.weight = weight;
+
+    }
 }
