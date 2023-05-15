ac7f435f961fe231686a112ea28e4f5f5c31c63e
BT_4N2_07 <tdukdak9287@bths.edu>
2023-05-15 12:15:17 -0400

    followed through a GUI tutorial for the whole double period and started to incorporate buttons and event listeners

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,11 @@ import javax.swing.border.Border;
 
 public class Main {
     public static void main(String[] args) {
-        ImageIcon image = new ImageIcon("src/bths.png");
-        Border border = BorderFactory.createLineBorder(Color.blue,3);
 
 
-        JLabel label = new JLabel();
-        label.setText("Brooklyn Technical High School Transcript: ");
-        label.setIcon(image);
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.TOP);
-        label.setForeground(new Color(0xFF000000, true));
-        label.setFont(new Font("Times New Roman",Font.PLAIN,20));
-        label.setIconTextGap(-25);
-        label.setBackground(Color.white);
-        label.setOpaque(true);
-        label.setBorder(border);
-        label.setVerticalAlignment(JLabel.TOP);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        //label.setBounds(50,50,400,400);
-
-        JFrame myFrame = new JFrame();
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myFrame.setSize(500,500);
-        //myFrame.setLayout(null);
+        MyFrame myFrame = new MyFrame();
         myFrame.setVisible(true);
-        myFrame.add(label);
+
         //myFrame.pack();
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,17 +1,73 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import  javax.swing.ImageIcon;
+import javax.swing.border.Border;
 
-public class MyFrame extends JFrame{
+public class MyFrame extends JFrame implements ActionListener {
+    JButton addCourse;
+    JButton studentInfo;
+
     MyFrame(){
+
+
+        ImageIcon image = new ImageIcon("src/bths.png");
+        Border border = BorderFactory.createLineBorder(Color.blue,3);
+        addCourse = new JButton();
+        addCourse.setBounds(100, 400, 200, 50);
+        addCourse.addActionListener(this);
+        addCourse.setText("Add course");
+        addCourse.setFocusable(false);
+        studentInfo = new JButton();
+        studentInfo.setBounds(500, 400, 200, 50);
+        studentInfo.addActionListener(this);
+        studentInfo.setText("View student info");
+        studentInfo.setFocusable(false);
+
+
+
+
+
+        JLabel label = new JLabel();
+        label.setText("Brooklyn Technical High School Transcript: ");
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(new Color(0xFF000000, true));
+        label.setFont(new Font("Times New Roman",Font.PLAIN,20));
+        label.setIconTextGap(-25);
+        label.setBackground(Color.white);
+        label.setOpaque(true);
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.TOP);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.add(addCourse);
+        label.add(studentInfo);
+
+
+
+
         this.setTitle("Transcript");
         this.setDefaultCloseOperation(MyFrame.EXIT_ON_CLOSE);
-        this.setResizable(true);
-        this.setSize(420,420);
+        this.setSize(800,1600);
+        this.setResizable(false);
         this.setVisible(true);
+        this.add(label);
 
-        ImageIcon image = new ImageIcon("bths.png");
+
+
         this.setIconImage(image.getImage());
-        this.getContentPane().setBackground(new Color(150, 75, 0));
+        this.getContentPane().setBackground(new Color(255,255,255));
     }
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==addCourse){
+            System.out.print("poo");
+        }
+        if(e.getSource()==studentInfo){
+            System.out.print("pee");
+        }
+    }
+
+
 }
