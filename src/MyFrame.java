485613caa45062e3c3e2a3123aab2b1065767a18
BT_4N2_07 <tdukdak9287@bths.edu>
2023-05-26 12:04:53 -0400

    changed the appearance of my buttons and textfield as final touches

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -13,8 +13,6 @@ public class MyFrame extends JFrame implements ActionListener {
     private JTextArea textArea1;
     private ArrayList<Course> courses = new ArrayList<Course>();
 
-
-
     MyFrame(){
         String studentName = JOptionPane.showInputDialog("Student Name?");
         String year = JOptionPane.showInputDialog("What year is the student");
@@ -22,21 +20,25 @@ public class MyFrame extends JFrame implements ActionListener {
         newStudent = new StudentInfo(studentName, year, onTrack);
 
         ImageIcon image = new ImageIcon("src/bths.png");
-        Border border = BorderFactory.createLineBorder(Color.blue,3);
+        Border border = BorderFactory.createLineBorder(new Color(137, 207, 240), 3);
         addCourse = new JButton();
         addCourse.setBounds(100, 400, 200, 50);
         addCourse.addActionListener(this);
         addCourse.setText("Add course");
+        addCourse.setBackground(new Color(137, 207, 240));
         addCourse.setFocusable(false);
         studentInfo = new JButton();
         studentInfo.setBounds(500, 400, 200, 50);
         studentInfo.addActionListener(this);
         studentInfo.setText("View student info");
+        studentInfo.setBackground(new Color(137, 207, 240));
         studentInfo.setFocusable(false);
         textArea1 = new JTextArea("");
-        textArea1.setLocation(4,500);
+        textArea1.setLocation(20,500);
         textArea1.setSize(new Dimension(750,500));
         textArea1.setFont(new Font("Times New Roman", Font.PLAIN,20));
+        textArea1.setBackground(new Color(137, 207, 240));
+
 
         JLabel label = new JLabel();
         label.setText("Brooklyn Technical High School Transcript: ");
@@ -55,15 +57,12 @@ public class MyFrame extends JFrame implements ActionListener {
         label.add(studentInfo);
         label.add(textArea1);
 
-
-
         this.setTitle("Transcript");
         this.setDefaultCloseOperation(MyFrame.EXIT_ON_CLOSE);
         this.setSize(800,1600);
         this.setResizable(false);
         this.setVisible(true);
         this.add(label);
-
 
         this.setIconImage(image.getImage());
         this.getContentPane().setBackground(new Color(255,255,255));
